f6c2aa7ac1a40c0c2a8c439b2061b7bc3aa6686e
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-09 16:27:27 +0400

    Add files via upload

---
+++ oddnumbers5.java
@@ -0,0 +1,11 @@
+public class oddnumbers5{
+	public static void main(String[] arg){
+		int a=0;
+		while (a<=9){
+			a++;
+			if((a%2!=0 && a!=5) && (a%2!=0 || a==5))
+				System.out.println(a);
+		}
+			
+	}
+}
